1c9f08a8bad7e8fa33cb5290c90a8ea23c187fd2
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-18 16:17:33 +0500

    Initial Commit

---
+++ BaccaratGame/BaccaratGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaccaratGameApp.swift
+//  BaccaratGame
+//
+//  Created by Faheeam Ahmed on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BaccaratGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
